e6fe8caab07f4ae9ea096e2def5c96a890546764
xt-el <notartem1@gmail.com>
2025-10-04 17:48:03 -0300

    Update Core.lua

--- modules/Core.lua
+++ modules/Core.lua
@@ -1,1 +1,14 @@
+local Core = {}
 
+function Core.start()
+
+-- TODO O SEU SCRIPT ORIGINAL VEM AQUI
+-- ...
+-- ... (todo o código da GUI, das funções e dos listeners)
+-- ...
+-- Mostrar GUI
+G2L["1"].Parent = core
+
+end
+
+return Core -- Retorna a função para o arquivo principal
